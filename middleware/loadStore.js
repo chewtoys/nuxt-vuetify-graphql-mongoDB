7b42dfe0dcc6b8b3d61ac01ca071ccce4bbacda8
Didicast Dev Leader <youngsoo.j@Didicastui-MacBook-Pro.local>
2019-03-29 18:36:02 +0900

    fix loadStore

--- middleware/loadStore.js
+++ middleware/loadStore.js
@@ -1,8 +1,9 @@
 import registerStoreModule from '../store/common'
 
-export default function({ store, app, req, route, params, env }) {
+export default function({ store, app, req, route, params, env, redirect }) {
   console.log('params :', params)
   console.log('store :', store)
+  console.log('route :', route)
   if (params && params.module) {
     const module = params.module
     console.log('loadStore > module :', module)
@@ -16,6 +17,16 @@ export default function({ store, app, req, route, params, env }) {
       }
       return true
     })
+
+    console.log('store.registerModule :', store.registerModule)
+    if (
+      !store.registerModule ||
+      store.registerModule === undefined ||
+      store.registerModule === 'undefined'
+    ) {
+      console.log('undefined registerModule of store > oute.path', route.path)
+      return redirect('/admin')
+    }
 
     const autoSchema = store.getters.getAutoSchema(module)
     if (!(store && store.state && store.getters[module + '/gqlTypes'])) {
